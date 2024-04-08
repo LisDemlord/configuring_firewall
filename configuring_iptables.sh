59957d6bb2d2b16b3c40aab8f8a38043a432e136
Lis Demlord <lis.demlord@gmail.com>
2024-04-08 14:06:13 +0500

    added new functions

--- configuring_iptables.sh
+++ configuring_iptables.sh
@@ -2,10 +2,21 @@
 [ $UID -eq 0 ] || 
 { echo "This script needs to be run with sudo or by root."; exit 1; }
 
-# Enable strict error checking
+# Включение строгой проверки на наличие ошибок
 set -o errexit
 set -o nounset
 set -o pipefail
+
+# Проверка наличия необходимых утилит
+check_dependencies() {
+    local dependencies='iptables osqueryi jq awk tr wc'
+    for dep in $dependencies; do
+        if ! command -v "$dep" >/dev/null 2>&1; then
+            echo "Error: $dep is not installed or not in PATH"
+            exit 1
+        fi
+    done
+}
 
 check_iptables() {
 	echo -e "STATUS IPTABLES:\n"
@@ -13,18 +24,29 @@ check_iptables() {
 }
 
 clear_iptables() {
-# Set default policies for INPUT and OUTPUT chains
+# Установка политики по умолчанию для цепочек ВВОДА и ВЫВОДА
 	iptables -P INPUT ACCEPT
 	iptables -P OUTPUT ACCEPT
 	
-	# Clear all rules
+	# Очистка всех правил
 	iptables -F
-	# Clear all user-defined chains
+	# Очистка всех пользовательских цепочек
 	iptables -X
-	# Clear all byte counters
+	# Сброс счетсичков пакетов и байтов
 	iptables -Z
 	
-	# Flush and destroy all IP sets
+	# Очистка и уничтожение всех наборов IP-адресов
 	ipset flush
 	ipset destroy
+}
+
+initial_setup_iptables() {
+	iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+	iptables -A INPUT -i lo -j ACCEPT
+	iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+	iptables -A INPUT -p icmp -j ACCEPT
+}
+
+change_base_policy() {
+	iptables -P INPUT DROP
 }
