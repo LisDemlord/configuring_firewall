dfe7edaf96f7965636ddb5e5a42f651e5557c397
Lis Demlord <lis.demlord@gmail.com>
2024-04-22 11:49:47 +0500

    added comments

--- configuring_iptables.sh
+++ configuring_iptables.sh
@@ -16,7 +16,7 @@ check_dependencies() {
     local dependencies='iptables osqueryi jq awk tr wc'
     for dep in $dependencies; do
         if ! command -v "$dep" >/dev/null 2>&1; then
-            echo "Error: $dep is not installed or not in PATH"
+            echo "Error: $dep is not installed or not in PATH..."
             exit 1
         fi
     done
@@ -29,44 +29,49 @@ check_iptables() {
 
 clear_iptables() {
     # Установка политики по умолчанию для цепочек ВВОДА и ВЫВОДА
-    iptables -P INPUT ACCEPT || { echo "Failed to set INPUT policy"; exit 1; }
-    iptables -P OUTPUT ACCEPT || { echo "Failed to set OUTPUT policy"; exit 1; }
+    iptables -P INPUT ACCEPT || { echo "ERROR! Failed to set INPUT policy..."; exit 1; }
+    iptables -P OUTPUT ACCEPT || { echo "ERROR! Failed to set OUTPUT policy..."; exit 1; }
     
     # Очистка всех правил
-    iptables -F || { echo "Failed to flush rules"; exit 1; }
+    iptables -F || { echo "ERROR! Failed to flush rules..."; exit 1; }
     # Очистка всех пользовательских цепочек
-    iptables -X || { echo "Failed to flush custom chains"; exit 1; }
+    iptables -X || { echo "ERROR! Failed to flush custom chains..."; exit 1; }
     # Сброс счетсичков пакетов и байтов
-    iptables -Z || { echo "Failed to zero counters"; exit 1; }
+    iptables -Z || { echo "ERROR! Failed to zero counters..."; exit 1; }
     
     # Очистка и уничтожение всех наборов IP-адресов
-    ipset flush || { echo "Failed to flush ipset"; exit 1; }
-    ipset destroy || { echo "Failed to destroy ipset"; exit 1; }
+    ipset flush || { echo "ERROR! Failed to flush ipset..."; exit 1; }
+    ipset destroy || { echo "ERROR! Failed to destroy ipset..."; exit 1; }
 }
 
 initial_setup_iptables() {
     # Добавление правил iptables
-    iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT || { echo "Failed to add RELATED,ESTABLISHED rule"; exit 1; }
-    iptables -A INPUT -i lo -j ACCEPT || { echo "Failed to add loopback rule"; exit 1; }
-    iptables -A INPUT -m conntrack --ctstate INVALID -j DROP || { echo "Failed to add INVALID rule"; exit 1; }
-    iptables -A INPUT -p icmp -j ACCEPT || { echo "Failed to add ICMP rule"; exit 1; }
+    iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT || { echo "ERROR! Failed to add RELATED,ESTABLISHED rule..."; exit 1; }
+    iptables -A INPUT -i lo -j ACCEPT || { echo "ERROR! Failed to add loopback rule..."; exit 1; }
+    iptables -A INPUT -m conntrack --ctstate INVALID -j DROP || { echo "ERROR! Failed to add INVALID rule..."; exit 1; }
+    iptables -A INPUT -p icmp -j ACCEPT || { echo "ERROR! Failed to add ICMP rule..."; exit 1; }
 }
 
 change_default_policy() {
+    # Функция для изменения политики по умолчанию для правил iptables
 	echo -e "\n"
-    read -r -p "Enter default policy (ACCEPT or DROP): " user_policy
+    read -r -p "Enter the default policy (ACCEPT or DROP): " user_policy
     case "$user_policy" in
         "ACCEPT" | "DROP")
-            iptables -P INPUT "$user_policy" || { echo "Failed to set default policy"; exit 1; }
+            # Если пользователь ввел ACCEPT или DROP, устанавливаем соответствующую политику
+            iptables -P INPUT "$user_policy" || { echo "ERROR! The default policy could not be set..."; exit 1; }
             ;;
         *)
-            echo "Invalid policy. Please enter either ACCEPT or DROP."
+            # Если введена некорректная политика, выводим сообщение об ошибке и завершаем выполнение
+            echo "ERROR! Incorrect policy. Please enter ACCEPT or DROP..."
             exit 1 ;;
     esac
 }
 
 data_collection() {
+    # Функция для сбора данных
     local ip_data_osquery
+    # Выполняем запрос к osquery для сбора данных об открытых сокетах
     ip_data_osquery=$(echo 'SELECT (
               CASE family 
               WHEN 2 THEN "IP4" 
@@ -81,39 +86,47 @@ data_collection() {
             FROM process_open_sockets 
             WHERE family IN (2) 
             AND protocol IN (6, 17) 
-            LIMIT 4;' | osqueryi --json) || { echo "Failed to collect data..."; exit 1; }
-
+            LIMIT 4;' | osqueryi --json) || { echo "ERROR! The data could not be collected..."; exit 1; }
+    
+    # Выводим собранные данные
     echo "$ip_data_osquery"
 }
 
 formatting_data() {
+    # Функция для форматирования данных
     local field="$1"
     local formatted_data
-    formatted_data=$(data_collection | jq -r ".[] | .$field") || { echo "Failed to format data..."; exit 1; }
-
+    # Получаем данные с помощью функции data_collection и обрабатываем их с помощью jq
+    formatted_data=$(data_collection | jq -r ".[] | .$field") || { echo "ERROR! The data could not be formatted..."; exit 1; }
+    
+    # Выводим отформатированные данные
     echo "$formatted_data"
 }
 
-
 formation_of_rules() {
+    # Функция для формирования правил iptables на основе собранных данных
     local net_family net_local_address net_local_port net_protocol net_remote_address
+    # Получаем данные о сетевых соединениях с помощью функции formatting_data
     net_family=$(formatting_data family)
     net_local_address=$(formatting_data local_address)
     net_local_port=$(formatting_data local_port)
     net_protocol=$(formatting_data protocol)
     net_remote_address=$(formatting_data remote_address)
 
+    # Проверяем, чтобы количество элементов в каждом поле было одинаковым
     if [ "$(wc -w <<< "$net_family")" -ne "$(wc -w <<< "$net_local_address")" ] ||
        [ "$(wc -w <<< "$net_family")" -ne "$(wc -w <<< "$net_local_port")" ] ||
        [ "$(wc -w <<< "$net_family")" -ne "$(wc -w <<< "$net_protocol")" ] ||
        [ "$(wc -w <<< "$net_family")" -ne "$(wc -w <<< "$net_remote_address")" ]; then
-        echo "Error: different number of items in the connection table"
+        echo "ERROR! Different number of items in the connection table..."
         exit 1
     fi
 
     local number_of_connections
+    # Получаем общее количество соединений
     number_of_connections=$(echo "$net_family" | wc -l)
 
+    # Проходим по каждому соединению и создаем соответствующее правило iptables
     for ((i = 1; i <= number_of_connections; i++)); do
         family=$(echo "$net_family" | awk "NR==$i")
         local_address=$(echo "$net_local_address" | awk "NR==$i")
@@ -121,44 +134,54 @@ formation_of_rules() {
         protocol=$(echo "$net_protocol" | awk "NR==$i")
         remote_address=$(echo "$net_remote_address" | awk "NR==$i")
 
-        echo -e "\nCreating iptables rules for connection $i:"
+        echo -e "\nCreating iptables rules for a connection $i:"
         echo "Family: $family"
-        echo "Local Address: $local_address"
-        echo "Local Port: $local_port"
+        echo "Local address: $local_address"
+        echo "Local port: $local_port"
         echo "Protocol: $protocol"
-        echo -e "Remote Address: $remote_address\n"
+        echo -e "Remote address: $remote_address\n"
         
-        iptables -A INPUT -p "$protocol" --dport "$local_port" -s "$remote_address" -d "$local_address" -j ACCEPT || { echo "Failed to create a rule..."; exit 1; }
+        # Создаем правило iptables для данного соединения
+        iptables -A INPUT -p "$protocol" --dport "$local_port" -s "$remote_address" -d "$local_address" -j ACCEPT || { echo "ERROR! Failed to create a rule..."; exit 1; }
     done
 }
 
 save_iptables_rules() {
-    iptables-save > "$rules_file_path" || { echo "Failed to save a rule..."; exit 1; }
+    # Функция для сохранения правил iptables
+    iptables-save > "$rules_file_path" || { echo "The rule could not be saved..."; exit 1; }
     echo -e "\niptables rules are saved in $rules_file_path\n"
 }
 
 create_restore_iptables() {
+    # Функция для создания скрипта восстановления правил iptables
     echo '#!/bin/bash' > "$restore_iptables_file_path"
     echo "iptables-restore < '$rules_file_path'" >> "$restore_iptables_file_path"
     echo 'exit 0' >> "$restore_iptables_file_path"
 }
 
 сreate_systemd_service() {
+    # Функция для создания systemd-службы
     cat <<EOF | tee "/etc/systemd/system/$service_name" > /dev/null
 	[Unit]
-	Description=My Script Service
+	Description=iptables rules service
 	After=network.target
+	
 	[Service]
 	Type=simple
 	ExecStart=$restore_iptables_file_path
+	
 	[Install]
 	WantedBy=multi-user.target
 EOF
 
+    # Перезагружаем конфигурацию systemd
     systemctl daemon-reload
-    systemctl enable "$service_name" || { echo "Failed to enable a service..."; exit 1; }
-    systemctl start "$service_name" || { echo "Failed to start a service..."; exit 1; }
+    # Включаем созданную службу
+    systemctl enable "$service_name" || { echo "ERROR! The service could not be enabled..."; exit 1; }
+    # Запускаем службу
+    systemctl start "$service_name" || { echo "ERROR! The service could not be started..."; exit 1; }
 }
+
 
 main() {
     check_dependencies
